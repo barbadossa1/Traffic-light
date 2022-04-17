b50ff8ced3b7b2a8a9a5c6c584d4d14a039430aa
kuznetsov.vadim <mr.kuznetsov@outlook.com>
2022-04-17 20:56:50 +0300

    constraints setup and code

--- Traffic lights - homework-2-1-2/ViewController.swift
+++ Traffic lights - homework-2-1-2/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {    
+    
+    @IBOutlet var redLightCircle: UIView!
+    @IBOutlet var yellowLightCircle: UIView!
+    @IBOutlet var greenLightCircle: UIView!
+    @IBOutlet var buttonSwitcher: UIButton!
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightCircle.layer.cornerRadius = redLightCircle.frame.size.width/2
+        yellowLightCircle.layer.cornerRadius = yellowLightCircle.frame.size.width/2
+        greenLightCircle.layer.cornerRadius = greenLightCircle.frame.size.width/2
+        
     }
-
-
+    
+    var someNumber: CGFloat = 0.3
+    
+    @IBAction func switchingTheLight() {
+        switch someNumber {
+        case redLightCircle.alpha:
+            redLightCircle.alpha = 0.3
+            yellowLightCircle.alpha = 1
+        
+        case yellowLightCircle.alpha:
+            yellowLightCircle.alpha = 0.3
+            greenLightCircle.alpha = 1
+            
+        case greenLightCircle.alpha:
+            greenLightCircle.alpha = 0.3
+            redLightCircle.alpha = 1
+            
+        default:
+            redLightCircle.alpha = 1
+            buttonSwitcher.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
